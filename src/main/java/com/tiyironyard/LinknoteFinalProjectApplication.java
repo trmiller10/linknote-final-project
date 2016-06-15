53600ff62e7f566942882b02376624209ec29c01
Taylor Miller <trmiller10@gmail.com>
2016-06-15 16:09:01 -0400

    created base project

---
+++ src/main/java/com/tiyironyard/LinknoteFinalProjectApplication.java
@@ -0,0 +1,12 @@
+package com.tiyironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinknoteFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinknoteFinalProjectApplication.class, args);
+	}
+}
